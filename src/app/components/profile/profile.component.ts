6b54a14d12920e6b4a3631314c8d442bc8826406
Mellisah-lisah <aliceochieng25@gmail.com>
2020-04-15 13:10:05 +0300

    final changes

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -8,19 +8,24 @@ import { ProfileService } from 'src/app/service/profile.service'
 })
 
 export class ProfileComponent implements OnInit {
-  profile:any[];
-  repos: any[];
+ profile:any;
+  repos: any;
+  username:string;
 
   constructor(private profileService:ProfileService) { 
+  
+  }
+  findProfile(){
+    this.profileService.updateProfile(this.username)
     this.profileService.getProfileInfo().subscribe(profile => {
       console.log(profile);
-      // this.profile = profile;
+      this.profile = profile;
 
     });
-    // this.profileService.getProfileRepos().subscribe(repos => {
-    //   console.log(repos);
-    //   this.repos = repos;
-    // })
+    this.profileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    })
   }
 
   ngOnInit(): void {
--- src/app/service/profile.service.ts
+++ src/app/service/profile.service.ts
@@ -4,20 +4,24 @@ import { HttpClient } from '@angular/common/http'
 @Injectable()
 
 export class ProfileService {
-  private username :string;
-  private Clientid  = ' 64f43088843952a17963';
+ 
+  private username: string;
+  private Clientid = ' 64f43088843952a17963';
   private Clientsecret = 'bed7a54e2aa776ae8e858d42c76224ac566b7fbf';
 
-  constructor(private http:HttpClient) {
-    console.log("it wotks");
-    this. username = 'Mellisah-lisah'
-   }
-   getProfileInfo(){
-     return this.http.get("https://api.github.com/users/" + this.username + "? client_id=" +this.Clientid + "&client_secret="+
- + this.Clientsecret)
+  constructor(private http: HttpClient) {
+    console.log("it works");
+    this.username = 'Mellisah-lisah'
   }
-  getProfileRepos(){
-    return this.http.get("https://api.github.com/users/" + this.username + "? client_id=" +this.Clientid + "&client_secret="+
- + this.Clientsecret)
+  getProfileInfo() {
+    return this.http.get("https://api.github.com/users/" + this.username + "? client_id=" + this.Clientid + "&client_secret=" +
+      + this.Clientsecret)
+  }
+  getProfileRepos() {
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?lient_id=" + this.Clientid + "&client_secret=" +
+      + this.Clientsecret)
+  }
+  updateProfile (username: string) {
+    this.username = username;
   }
 }
